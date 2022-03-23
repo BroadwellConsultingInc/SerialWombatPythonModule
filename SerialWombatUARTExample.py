0b73d574eb8cb580c809aac8aed99acfb6299171
broadwellconsultinginc <jon@broadwell.consulting>
2022-03-23 16:33:17 +0000

    Updated UART example to match video

--- SerialWombatUARTExample.py
+++ SerialWombatUARTExample.py
@@ -1,12 +1,6 @@
 import time
-import RPi.GPIO as GPIO
 import SerialWombatUART
-import SerialWombatServo
-import SerialWombatAnalogInput
-import SerialWombatQuadEnc
-
-
-GPIO.setwarnings(False)
+import SerialWombatUltrasonicDistanceSensor
 
 sw = SerialWombatUART.SerialWombatChipUART("/dev/ttyUSB0")
 
@@ -17,25 +11,12 @@ print(sw.model)
 print(sw.fwVersion)
 
 
+distanceSensor = SerialWombatUltrasonicDistanceSensor.SerialWombatUltrasonicDistanceSensor(sw)
 
-analog0 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog1 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog2 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog3 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog4 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog16 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog17 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog18 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog19 = SerialWombatAnalogInput.SerialWombatAnalogInput(sw)
-analog0.begin(0)
-analog1.begin(1)
-analog2.begin(2)
-analog3.begin(3)
-analog4.begin(4)
-analog16.begin(16)
-analog17.begin(17)
-analog18.begin(18)
-analog19.begin(19)
+distanceSensor.begin(10, # echo pin
+                     0, # HC_SR04 driver
+                     11) # Trigger pin
+
 
 
 
@@ -44,5 +25,4 @@ print("Source Voltage mv: ",sw.readSupplyVoltage_mV())
 
 time.sleep(2)
 while(True):
-    print("0: ",analog0.readCounts(), " 1: ", analog1.readCounts(), " 2: ", analog2.readCounts(), " 3: ", analog3.readCounts(), " 4: ", analog4.readCounts(),
-          " 16: ", analog16.readCounts(), " 17: ", analog17.readCounts(), " 18: ", analog18.readCounts(), " 19: ", analog19.readCounts())
+    print("Distance (mm): ",distanceSensor.readPublicData()," Pulse count: ",distanceSensor.readPulseCount())
